f3b0af8b0b708ceeeb6887f4cd4fcc89ce95258e
Yudai915 <40430090+Yudai915@users.noreply.github.com>
2019-08-31 23:58:27 +0900

    webリクエストタイミングとloop周期変更

--- DQBot/MyDQDiscord.py
+++ DQBot/MyDQDiscord.py
@@ -21,10 +21,10 @@ class TengokuNotice(commands.Cog):
     def Set_ch(self,ch : discord.channel):
         self.channel = ch
 
-    @tasks.loop(seconds=20)
+    @tasks.loop(seconds=30)
     async def Update(self):
         now = datetime.now()
-        if now.minute == 0 and self.open == False:
+        if now.minute >= 0 and now.minute <= 5 and self.open == False:
             soup = BeautifulSoup(requests.get(TengokuNotice.URL).content,"html.parser")
             div = soup.find("div", class_="tengoku__period")
             tengokutext = div.text
